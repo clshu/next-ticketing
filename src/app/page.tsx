2164113293df4b4d18ad1e5bd5929187698a498e
clshu <clshu2020@gmail.com>
2025-01-26 12:06:06 -0800

    Format Dashboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import TicketCard from '@/components/TicketCard'
 
 export default function Dashboard() {
   return (
-    <div>
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
+    <div className="p-4">
+      <div className="lg:grid grid-cols-2 xl:grid grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
   )
 }
