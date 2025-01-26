428e42b36ffd0b21561fde38b3e0bb5dc780127b
clshu <clshu2020@gmail.com>
2025-01-26 13:22:33 -0800

    Set up mongodb and mongoose. Create Ticket schema and connect to mongodb

---
+++ src/models/Ticket.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose'
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ticket'
+
+mongoose.connect(uri)
+mongoose.Promise = global.Promise
+
+export interface ITicket extends mongoose.Document {
+  title: string
+  description: string
+  category: string
+  priority: number
+  progress: number
+  status: string
+  active: boolean
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+const TicketSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    priority: { type: Number, required: true },
+    progress: { type: Number, required: true },
+    status: { type: String, required: true },
+    active: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+)
+
+const Ticket =
+  mongoose.models.Tecket || mongoose.model<ITicket>('Ticket', TicketSchema)
+
+export default Ticket
