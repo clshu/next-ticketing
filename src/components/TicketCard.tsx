5a03a26c02b93870a19617034088624fc4e9e4ce
clshu <clshu2020@gmail.com>
2025-01-26 11:41:02 -0800

    Add StatusDisplay component. Format TicketCard.

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -1,13 +1,22 @@
 import DeleteBlock from '@/components/DeleteBlock'
 import PriorityDisplay from '@/components/PriorityDisplay'
 import ProgressDisplay from '@/components/ProgressDisplay'
+import StatusDisplay from '@/components/StatusDisplay'
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col  bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4> Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">Ticket descriptionn</p>
       <ProgressDisplay />
+      <StatusDisplay />
     </div>
   )
 }
