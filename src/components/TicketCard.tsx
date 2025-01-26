6cde37dbe13df1804bc9c3bb100b631a7ac08cc5
clshu <clshu2020@gmail.com>
2025-01-26 11:21:29 -0800

    Add PriorityDisplay and ProgressDisplay components

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -1,9 +1,13 @@
 import DeleteBlock from '@/components/DeleteBlock'
+import PriorityDisplay from '@/components/PriorityDisplay'
+import ProgressDisplay from '@/components/ProgressDisplay'
 
 const TicketCard = () => {
   return (
     <div>
       <DeleteBlock />
+      <PriorityDisplay />
+      <ProgressDisplay />
     </div>
   )
 }
